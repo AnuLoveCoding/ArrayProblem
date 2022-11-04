9a2310bf0d55a38a894c7d0c7c61f482bee99703
anuLoveCoading <anusrivas07@gmail.com>
2022-11-04 23:33:42 +0530

    git commit -m "ArrayProblem"

--- src/pb3.java
+++ src/pb3.java
@@ -1,22 +1,43 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class pb3 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        System.out.println("Enter Your Number arr Size  ");
-        int n = scn.nextInt();
 
-        int [] arr = new int[n];
+//       Q. 1 Take n values from user denoting marks of n students. Store these values
+//         in array and then find the average score.
 
-        System.out.println("Enter number for "+ n +" arr storeing;");
-        for (int i = 0; i < arr.length; i++) {
-            arr[i]= scn.nextInt();
-        }
+            System.out.println("Enter Your Marks  size ");
+            int n = scn.nextInt();
+            int [] arr = new int[n];
 
-        System.out.println("Here are your Output");
-        for (int j : arr) {
-            System.out.print(j + " ");
-        }
+            System.out.println("Enter Your marks");
+            for (int i = 0; i < arr.length ; i++)  arr[i] = scn.nextInt();
+
+            double  sum =0 ;
+
+            for (int j : arr) sum += j;
+
+            double  ans  = (double) sum/arr.length;
+            System.out.println(ans);
+
+
+
+//        Q.2  print the array and declare the array;
+
+//        System.out.println("Enter Your Number arr Size  ");
+//
+//        int n = scn.nextInt() ; int [] arr = new int[n] ;
+//
+//        System.out.println("Enter number for "+ n +" arr storing;");
+//        for (int i = 0; i < arr.length; i++) arr[i] = scn.nextInt();
+//
+//        for (int j : arr) System.out.println("arr["+j+"] = "+j+";");
+
+
+
+
 
 
     }
