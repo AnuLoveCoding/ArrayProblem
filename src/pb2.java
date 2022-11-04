58a13efe85331e206fb32831c76db019fa125a90
anuLoveCoading <anusrivas07@gmail.com>
2022-11-04 15:53:09 +0530

    git commit -m "ArrayProblem"

--- src/pb2.java
+++ src/pb2.java
@@ -2,19 +2,47 @@ import java.util.Scanner;
 
 public class pb2 {
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        System.out.println("Enter Your n number");
-//        int n = scn.nextInt();
-           int [] arr = new int[5];
-//        int [] arr= new int[n];
+
+        int[]arr = {10,20,30,40,50};
+
+        arr[3] = 95;
+
+        arr[4] = 60;
 
 
-        for (int i = 0; i < 5 ; i++) {
-            arr[i] = scn.nextInt();
+
+        for(int i=0; i < arr.length;i++) {
+
+            System.out.print(arr[i] + " ");
+
         }
-        for (int i = 0; i < 5 ; i++) {
-            System.out.println(arr[i]);
-        }
+//        Scanner scn = new Scanner(System.in);
+//        System.out.println("Enter Your n number");
+////        int n = scn.nextInt();
+//           int [] arr = new int[5];
+////        int [] arr= new int[n];
+//
+//        for (int i = 0; i < 5; i++) {
+//            arr[i] = scn.nextInt();
+//        }
+//
+//        double sum =0, count= 0;
+//        for (int i = 0; i < arr.length ; i++) {
+//            sum+=i;
+//            count++;
+//        }
+//
+//        double ans = (double) sum/count;
+//        System.out.println(ans);
+
+
+//        for (int i = 0; i < 5 ; i++) {
+//            arr[i] = scn.nextInt();
+//        }
+//        for (int i = 0; i < 5 ; i++) {
+//            System.out.println(arr[i]);
+//        }
+
 
 //        System.out.println(Arrays);
     }
