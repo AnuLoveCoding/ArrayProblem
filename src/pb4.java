9e63f179c9e9b42331c5f9a470ada995cbb7d3c2
anuLoveCoading <anusrivas07@gmail.com>
2022-11-05 21:48:55 +0530

    git commit -m "ArrayProblem"

--- src/pb4.java
+++ src/pb4.java
@@ -1,32 +1,51 @@
+import com.sun.security.jgss.GSSUtil;
+
 import java.util.Scanner;
 
 public class pb4 {
+//    Q.1
+//    public static boolean Search_value(int[] arr , int k) {
+//           int ans = arr.length;
+//        for (int i = 0; i < ans; i++) {
+//            if(arr[i]==k){
+//                System.out.println(i);
+//                return  true;
+//            }
+//        }
+//        return false;
+//    }
 
-    public static boolean Search_value(int[] arr , int k) {
-           int ans = arr.length;
-        for (int i = 0; i < ans; i++) {
+//    Q.2
+    public static int count_freq(int [] arr , int k){
+        int count =0;
+
+        for (int i = 0; i <arr.length ; i++) {
             if(arr[i]==k){
-                System.out.println(i);
-                return  true;
+                count++;
             }
         }
-        return false;
+        return count;
     }
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        System.out.println("Enter Your Number");
-        int n = scn.nextInt();
+//        System.out.println("Enter Your Number");
+//        int n = scn.nextInt();
+//        int [] arr = new int[n];
 
-        int [] arr = new int[n];
-
-        System.out.println("Enter Your arr Value");
-        for (int i = 0; i < arr.length ; i++) {
-            arr[i] = scn.nextInt();
-        }
-        int k = 12;
+//        System.out.println("Enter Your arr Value");
+//        for (int i = 0; i < arr.length ; i++) {
+//            arr[i] = scn.nextInt();
+//        }
+//        Q.1;
+//        int k = 12;
 //         int [] arr = {20,15,12,6,10};
-        System.out.println(Search_value(arr,k));
+//        System.out.println(Search_value(arr,k));
+
+//        Q.2;
+        int [] arr = {12,6,15,6,12,8,16,87,25,66};
+         int k = 8;
+        System.out.println("count of count_freq"+count_freq(arr,k));
 
 
 
