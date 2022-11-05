4fa1e472eca99cb6fb2fd90dbcf41bb27d94974a
anuLoveCoading <anusrivas07@gmail.com>
2022-11-05 21:57:22 +0530

    git commit -m "ArrayProblem"

--- src/pb4.java
+++ src/pb4.java
@@ -16,15 +16,25 @@ public class pb4 {
 //    }
 
 //    Q.2
-    public static int count_freq(int [] arr , int k){
-        int count =0;
+//    public static int count_freq(int [] arr , int k){
+//        int count =0;
+//
+//        for (int i = 0; i <arr.length ; i++) {
+//            if(arr[i]==k){
+//                count++;
+//            }
+//        }
+//        return count;
+//    }
 
+//    Q.3
+    public static  boolean consecutive(int[] arr ){
         for (int i = 0; i <arr.length ; i++) {
-            if(arr[i]==k){
-                count++;
+            if(arr[i]==arr[i+1]){
+                return true;
             }
         }
-        return count;
+        return false;
     }
 
     public static void main(String[] args) {
@@ -43,10 +53,14 @@ public class pb4 {
 //        System.out.println(Search_value(arr,k));
 
 //        Q.2;
-        int [] arr = {12,6,15,6,12,8,16,87,25,66};
-         int k = 8;
-        System.out.println("count of count_freq"+count_freq(arr,k));
+//        int [] arr = {12,6,15,6,12,8,16,87,25,66};
+//         int k = 8;
+//        System.out.println("count of count_freq"+count_freq(arr,k));
 
+
+//        Q.3;
+          int [] arr = {4, 19,19,8};
+          System.out.println(consecutive(arr));
 
 
     }
