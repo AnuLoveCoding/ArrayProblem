0472bfcbdd32629cbb347883c81d61e8fea85ac0
anuLoveCoading <anusrivas07@gmail.com>
2022-11-07 17:53:00 +0530

    git commit -m "ArrayProblem"

--- src/pb6.java
+++ src/pb6.java
@@ -1,5 +1,75 @@
+import java.util.Scanner;
+
 public class pb6 {
+//    (Q.1) find the element in array function;
+//    public static boolean  search(int [] arr , int k){
+//        for (int i = 0; i <arr.length ; i++) {
+//            if (arr[i]==k){
+//                return true;
+//            }
+//        }
+//
+//        return false;
+//    }
+
+//    (Q.2)  find the frequency of element;
+//    public static int frequency(int [] arr , int k){
+//        int count =0;
+//
+//        for (int i = 0; i <arr.length ; i++) {
+//            if(arr[i]==k){
+//                count++;
+//            }
+//        }
+//
+//        return count;
+//    }
+
     public static void main(String[] args) {
-        System.out.println("welcome Array pb6");
+        Scanner scn = new Scanner(System.in);
+//        System.out.println("Enter Your arr size");
+//        int  n = scn.nextInt();
+
+//        (Q.1) find the element in array ;
+
+//        int [] arr = new int[n];
+//
+//        System.out.println("Enter Your arr value");
+//        for (int i = 0; i <arr.length ; i++) {
+//            arr[i] = scn.nextInt();
+//        }
+//        System.out.println("Enter value to search");
+//        int k= scn.nextInt();
+//
+//        System.out.println(search(arr,k));
+
+//        (Q.2) find the frequency of element;
+//        System.out.println("Enter Your arr size");
+//        int  n = scn.nextInt();
+//        int arr [] = new int[n];
+//
+//        System.out.println("Enter Your arr value");
+//        for (int i = 0; i <arr.length ; i++) {
+//            arr[i] = scn.nextInt();
+//        }
+//
+//        System.out.println("Enter value to search");
+//        int k = scn.nextInt();
+
+//        int count =0;
+//
+//        for (int i = 0; i <arr.length ; i++) {
+//            if(arr[i]==k){
+//                count++;
+//            }
+//        }
+//
+//        System.out.println(count);
+
+//        System.out.println(frequency(arr,k));
+
+//        (Q.3) find the consequent number ;
+
+
     }
 }
