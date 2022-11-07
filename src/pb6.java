2fdd7d362541e20a1c65eea77ad166af5338bce3
anuLoveCoading <anusrivas07@gmail.com>
2022-11-07 23:29:47 +0530

    git commit -m "ArrayProblem"

--- src/pb6.java
+++ src/pb6.java
@@ -162,54 +162,111 @@ public class pb6 {
 
 //        (Q.6) sum of Two arrays ;
 
-        System.out.println("Enter Your arr1 size");
-        int  n1 = scn.nextInt();
-        int [] arr1 = new int[n1];
-        System.out.println("Enter Your arr1 value");
-        for (int i = 0; i <arr1.length ; i++) {
-            arr1[i] = scn.nextInt();
-        }
+//        System.out.println("Enter Your arr1 size");
+//        int  n1 = scn.nextInt();
+//        int [] arr1 = new int[n1];
+//        System.out.println("Enter Your arr1 value");
+//        for (int i = 0; i <arr1.length ; i++) {
+//            arr1[i] = scn.nextInt();
+//        }
+//
+//        System.out.println("Enter Your arr2 size");
+//        int  n2 = scn.nextInt();
+//        int [] arr2 = new int[n2];
+//        System.out.println("Enter Your arr value");
+//        for (int i = 0; i <arr2.length ; i++) {
+//            arr2[i] = scn.nextInt();
+//        }
+//
+//        int [] sum = new int [Math.max(n1, n2)]; //[n1>n2 ? n1 : n2];
+//        int c =0 ;
+//
+//        int  i = arr1.length-1;
+//        int j = arr2.length-1;
+//        int  k= sum.length-1;
+//
+//        while(k>= 0){
+//            int d =c;
+//            if(i>=0){
+//                d+=arr1[0];
+//            }
+//            if(j>=0){
+//                d+=arr2[0];
+//            }
+//             c= d/10;
+//            d = d%10;
+//
+//            sum[k] =d;
+//            i--;
+//            j--;
+//            k--;
+//        }
+//        if(c!=0){
+//            System.out.println(c);
+//        }
+//
+//        for ( int val:sum) {
+//            System.out.println(val);
+//        }
 
-        System.out.println("Enter Your arr2 size");
-        int  n2 = scn.nextInt();
-        int [] arr2 = new int[n2];
-        System.out.println("Enter Your arr value");
-        for (int i = 0; i <arr2.length ; i++) {
-            arr2[i] = scn.nextInt();
-        }
+//        (Q.8) Difference Two Array;
 
-        int [] sum = new int [Math.max(n1, n2)]; //[n1>n2 ? n1 : n2];
-        int c =0 ;
-
-        int  i = arr1.length-1;
-        int j = arr2.length-1;
-        int  k= sum.length-1;
-
-        while(k>= 0){
-            int d =c;
-            if(i>=0){
-                d+=arr1[0];
-            }
-            if(j>=0){
-                d+=arr2[0];
-            }
-             c= d/10;
-            d = d%10;
-
-            sum[k] =d;
-            i--;
-            j--;
-            k--;
-        }
-        if()
-
-
-
-
-
-
-
-
+//        System.out.println("Enter Your arr1 size");
+//        int  a1 = scn.nextInt();
+//        int [] arr1 = new int[a1];
+//        System.out.println("Enter Your arr1 value");
+//        for (int i = 0; i <arr1.length ; i++) {
+//            arr1[i] = scn.nextInt();
+//        }
+//
+//        System.out.println("Enter Your arr2 size");
+//        int  a2 = scn.nextInt();
+//        int [] arr2 = new int[a2];
+//        System.out.println("Enter Your arr value");
+//        for (int i = 0; i <arr2.length ; i++) {
+//            arr2[i] = scn.nextInt();
+//        }
+//
+//        //Exception is a2 is greater than a1;
+//        int  [] diff = new int[a2];
+//        int c = 0;
+//
+//        int  i = arr1.length-1;
+//        int  j = arr2.length-1;
+//        int k = diff.length-1;
+//
+//        while(k>=0){
+//            int  d =0;
+//            int a1v =  i>=0 ? arr1[i] : 0;
+//            if(arr2[j] + c >= a1v){
+//                d = arr2[j]+c -a1v;
+//                c =0;
+//           }
+//            else{
+//                d = arr2[j] + c+ 10 - a1v;
+//                c =-1;
+//
+//            }
+//
+//            diff[k] =d;
+//            i--;
+//            j--;
+//            k--;
+//        }
+//
+//        int idx = 0;
+//        while(idx<diff.length){
+//            if(diff[idx]==0){
+//                idx++;
+//            }else{
+//                break;
+//            }
+//        }
+//
+//        while(idx < diff.length){
+//            System.out.println(0);
+//            idx++;
+//        }
 
     }
 }
