38d67c4825f68b7f0a51a714b869763b7ffee56b
anuLoveCoading <anusrivas07@gmail.com>
2022-11-09 11:37:14 +0530

    git commit -m "ArrayProblem"

--- src/problem7.java
+++ src/problem7.java
@@ -2,44 +2,53 @@ import java.util.Scanner;
 
 public class problem7 {
 //    (Q.1) sorted array; check array increasing manner or not
-    public static boolean check_increasing(int [] arr){
-        for (int i = 0; i <arr.length-1 ; i++) {
-            if(arr[i] < arr[i+1]){
-                continue; // you can skip to continue
-            } else  {
-                return false;
-            }
-        }
-        return true;
-    }
+//    public static boolean check_increasing(int [] arr){
+//        for (int i = 0; i <arr.length-1 ; i++) {
+//            if(arr[i] < arr[i+1]){
+//                continue; // you can skip to continue
+//            } else  {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
 //    (Q.2) find the single element in arr1;
-    public static int single_element(int [] arr1){
+//    public static int single_element(int [] arr1){
+//
+//        for (int i = 0; i < arr1.length ; i++) {
+//            int val = arr1[i];
+//            int freq = 0;
+//            for (int j = 0; j < arr1.length ; j++) {
+//                if(arr1[j]==val){
+//                    freq++;
+//                }
+//            }
+//            if(freq==1){
+//                return val;
+//            }
+//        }
+//
+//        return -1;
+//    }
+    public static int [] getConcatenation(int[] nums) {
+        int [] concatenation_array = new int [ 2* nums . length];
 
-        for (int i = 0; i < arr1.length ; i++) {
-            int val = arr1[i];
-            int freq = 0;
-            for (int j = 0; j < arr1.length ; j++) {
-                if(arr1[j]==val){
-                    freq++;
-                }
-            }
-            if(freq==1){
-                return val;
-            }
+        for(int i=0;i<nums.length;i++){
+            concatenation_array[i+nums.length] = concatenation_array[i]=nums[i];
         }
-
-        return -1;
+        return concatenation_array;
     }
     public static void main(String[] args) {
         Scanner  scn = new Scanner(System.in);
 //    (Q.1) sorted array; check array increasing manner or not
-        int [] arr = {1,2,3,4,5,6,7,8,9,};
-        System.out.println(check_increasing(arr));
+//        int [] arr = {1,2,3,4,5,6,7,8,9,};
+//        System.out.println(check_increasing(arr));
 
 //        (Q.2) given an array
+//        int [] arr1 = {3,9,14,16,3,14,16};
 
-        int [] arr1 = {3,9,14,16,3,14,16};
-
+        int [] nums = {1,2,1};
+        System.out.println(getConcatenation(nums));
 
     }
 }
